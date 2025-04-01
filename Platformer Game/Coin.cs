d0f5d93faca30bc100f924d5f426db7281dbcdf0
Naz Karazeybek <Naz Karazeybek@WIN-BMIHS2IRB2E>
2025-04-01 18:30:01 -0400

    Add project files.

---
+++ Platformer Game/Coin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Platformer_Game
+{
+    class Coin
+    {
+        private PictureBox coinPictureBox = new PictureBox();
+        public Coin()
+        {
+            coinPictureBox.Width = 10;
+            coinPictureBox.Height = 10;
+            coinPictureBox.BackColor = Color.Gold;
+        }
+
+        public void drawTo(Form F)
+        {
+            F.Controls.Add(coinPictureBox);
+        }
+
+        public Rectangle getBounds()
+        {
+            return coinPictureBox.Bounds;
+        }
+
+        public void setPos(int x, int y)
+        {
+            coinPictureBox.Location = new Point(x, y);
+        }
+    }
+}
